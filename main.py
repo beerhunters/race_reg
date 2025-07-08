7a9695a4762962ed432bf9d5be5708e53aac46f2
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-07-08 17:26:35 +0300

    It's almost done

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from aiogram.enums import ParseMode
 
 from database import init_db
 
-from handlers import register_handlers
+# from handlers import register_handlers
 
-# from handler_register import register_all_handlers
+from handler_register import register_all_handlers
 
 
 class CustomRotatingFileHandler(logging.handlers.BaseRotatingHandler):
@@ -92,8 +92,8 @@ dp = Dispatcher(storage=MemoryStorage())
 async def main():
     logger.info("Запуск бота")
     init_db()
-    register_handlers(dp, bot, ADMIN_ID)
-    # register_all_handlers(dp, bot, ADMIN_ID)
+    # register_handlers(dp, bot, ADMIN_ID)
+    register_all_handlers(dp, bot, ADMIN_ID)
     await bot.set_my_commands(
         [
             BotCommand(command="/start", description="Run, drink, repeat!"),
