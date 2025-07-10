91c1a06f8458d855279a7951b29fa198655d96f9
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-07-10 14:26:07 +0300

    It's almost done

--- handlers/admin_participant_handlers.py
+++ handlers/admin_participant_handlers.py
@@ -6,8 +6,9 @@ import pytz
 from aiogram import Dispatcher, Bot, F
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest
 from aiogram.filters import Command
-from aiogram.types import Message, BufferedInputFile
-from .utils import logger, messages, config
+from aiogram.types import Message, BufferedInputFile, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from .utils import logger, messages, config, RegistrationForm
 from database import (
     get_all_participants,
     get_pending_registrations,
@@ -24,10 +25,22 @@ from database import (
 def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int):
     logger.info("Регистрация обработчиков управления участниками")
 
-    @dp.message(Command("participants", "список", "участники"))
-    async def show_participants(message: Message):
-        logger.info(f"Команда /participants от user_id={message.from_user.id}")
+    async def show_participants(event: [Message, CallbackQuery]):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["set_reg_end_date_access_denied"])
+            return
+        logger.info(f"Команда /participants от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
         participants = get_all_participants()
+        if not participants:
+            await message.answer(messages["pending_empty"])
+            return
         participant_list = (
             messages["participants_list_header"] + messages["runners_header"]
         )
@@ -54,8 +67,6 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
                 else:
                     current_chunk += messages["volunteers_header"]
             last_role = role
-            # date_obj = datetime.datetime.fromisoformat(reg_date.replace("Z", "+00:00"))
-            # formatted_date = date_obj.strftime("%d.%m.%Y %H:%M")
             date_obj = datetime.datetime.fromisoformat(reg_date.replace("Z", "+00:00"))
             utc_timezone = pytz.timezone("UTC")
             moscow_timezone = pytz.timezone("Europe/Moscow")
@@ -72,7 +83,7 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
                     role=role,
                     date=formatted_date,
                     status=status_emoji,
-                    username=username,
+                    username=username or "не указан",
                     bib_number=bib_field,
                 )
             else:
@@ -83,7 +94,7 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
                     target_time=target_time,
                     role=role,
                     date=formatted_date,
-                    username=username,
+                    username=username or "не указан",
                     bib_number=bib_field,
                 )
             if len(current_chunk) + len(participant_info) > 4000:
@@ -97,16 +108,29 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
         chunks.append(current_chunk)
         for chunk in chunks:
             await message.answer(chunk)
+        if isinstance(event, CallbackQuery):
+            await event.answer()
 
-    @dp.message(Command("pending"))
-    async def show_pending_registrations(message: Message):
-        logger.info(f"Команда /pending от user_id={message.from_user.id}")
-        if message.from_user.id != admin_id:
-            logger.warning(
-                f"Доступ к /pending запрещен для user_id={message.from_user.id}"
-            )
-            await message.answer(messages["pending_access_denied"])
+    @dp.message(Command("participants", "список", "участники"))
+    async def cmd_show_participants(message: Message):
+        await show_participants(message)
+
+    @dp.callback_query(F.data == "admin_participants")
+    async def callback_show_participants(callback_query: CallbackQuery):
+        await show_participants(callback_query)
+
+    async def show_pending_registrations(event: [Message, CallbackQuery]):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["pending_access_denied"])
             return
+        logger.info(f"Команда /pending от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
         pending_users = get_pending_registrations()
         if not pending_users:
             await message.answer(messages["pending_empty"])
@@ -144,9 +168,26 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
         for chunk in chunks:
             await message.answer(chunk, parse_mode="HTML")
 
-    @dp.message(Command("stats", "статистика"))
-    async def show_stats(message: Message):
-        logger.info(f"Команда /stats от user_id={message.from_user.id}")
+    @dp.message(Command("pending"))
+    async def cmd_show_pending_registrations(message: Message):
+        await show_pending_registrations(message)
+
+    @dp.callback_query(F.data == "admin_pending")
+    async def callback_show_pending_registrations(callback_query: CallbackQuery):
+        await show_pending_registrations(callback_query)
+
+    async def show_stats(event: [Message, CallbackQuery]):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["pending_access_denied"])
+            return
+        logger.info(f"Команда /stats от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
         try:
             with sqlite3.connect("/app/data/race_participants.db", timeout=10) as conn:
                 cursor = conn.cursor()
@@ -175,20 +216,32 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
             logger.error(f"Ошибка при получении статистики: {e}")
             await message.answer("Ошибка при получении статистики. Попробуйте снова.")
 
-    @dp.message(Command("paid"))
-    async def mark_as_paid(message: Message):
-        logger.info(f"Команда /paid от user_id={message.from_user.id}")
-        if message.from_user.id != admin_id:
-            logger.warning(
-                f"Доступ к /paid запрещен для user_id={message.from_user.id}"
-            )
-            await message.answer(messages["paid_access_denied"])
+    @dp.message(Command("stats", "статистика"))
+    async def cmd_show_stats(message: Message):
+        await show_stats(message)
+
+    @dp.callback_query(F.data == "admin_stats")
+    async def callback_show_stats(callback_query: CallbackQuery):
+        await show_stats(callback_query)
+
+    async def mark_as_paid(event: [Message, CallbackQuery], state: FSMContext):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["paid_access_denied"])
             return
-        parts = message.text.split()
-        if len(parts) != 2 or not parts[1].isdigit():
-            await message.answer(messages["paid_usage"])
-            return
-        user_id = int(parts[1])
+        logger.info(f"Команда /paid от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
+        await message.answer(messages["wait_paid_ID"])
+        await state.set_state(RegistrationForm.waiting_for_paid_id)
+
+    @dp.message(RegistrationForm.waiting_for_paid_id)
+    async def process_mark_as_paid(message: Message, state: FSMContext):
+        user_id = int(message.text)
         participant = get_participant_by_user_id(user_id)
         if participant:
             update_payment_status(user_id, "paid")
@@ -230,21 +283,105 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
         else:
             await message.answer("Участник не найден.")
 
-    @dp.message(Command("set_bib"))
-    async def set_bib(message: Message):
-        logger.info(f"Команда /set_bib от user_id={message.from_user.id}")
-        if message.from_user.id != admin_id:
-            logger.warning(
-                f"Доступ к /set_bib запрещен для user_id={message.from_user.id}"
-            )
-            await message.answer(messages["set_bib_access_denied"])
+    @dp.message(Command("paid"))
+    async def cmd_mark_as_paid(message: Message, state: FSMContext):
+        await mark_as_paid(message, state)
+
+    @dp.callback_query(F.data == "admin_paid")
+    async def callback_mark_as_paid(callback_query: CallbackQuery, state: FSMContext):
+        await mark_as_paid(callback_query, state)
+
+    # @dp.message(Command("set_bib"))
+    # async def set_bib(message: Message):
+    #     logger.info(f"Команда /set_bib от user_id={message.from_user.id}")
+    #     if message.from_user.id != admin_id:
+    #         logger.warning(
+    #             f"Доступ к /set_bib запрещен для user_id={message.from_user.id}"
+    #         )
+    #         await message.answer(messages["set_bib_access_denied"])
+    #         return
+    #     parts = message.text.split()
+    #     if len(parts) != 3 or not parts[1].isdigit() or not parts[2].isdigit():
+    #         await message.answer(messages["set_bib_usage"])
+    #         return
+    #     user_id = int(parts[1])
+    #     bib_number = int(parts[2])
+    #     if bib_number <= 0:
+    #         await message.answer(messages["set_bib_invalid"])
+    #         return
+    #     participant = get_participant_by_user_id(user_id)
+    #     if participant:
+    #         success = set_bib_number(user_id, bib_number)
+    #         if success:
+    #             await message.answer(
+    #                 messages["set_bib_success"].format(
+    #                     name=participant[2], bib_number=bib_number
+    #                 )
+    #             )
+    #             try:
+    #                 await bot.send_message(
+    #                     chat_id=user_id,
+    #                     text=messages["bib_number_assigned"].format(
+    #                         bib_number=bib_number
+    #                     ),
+    #                 )
+    #                 logger.info(
+    #                     f"Уведомление о присвоении номера {bib_number} отправлено пользователю user_id={user_id}"
+    #                 )
+    #             except TelegramForbiddenError:
+    #                 logger.warning(f"Пользователь user_id={user_id} заблокировал бот")
+    #                 delete_participant(user_id)
+    #                 delete_pending_registration(user_id)
+    #                 logger.info(
+    #                     f"Пользователь user_id={user_id} удалён из таблиц participants и pending_registrations"
+    #                 )
+    #                 try:
+    #                     await bot.send_message(
+    #                         chat_id=admin_id,
+    #                         text=messages["admin_blocked_notification"].format(
+    #                             name=participant[2],
+    #                             username=participant[1] or "не указан",
+    #                             user_id=user_id,
+    #                         ),
+    #                     )
+    #                     logger.info(
+    #                         f"Уведомление администратору (admin_id={admin_id}) о блокировке отправлено"
+    #                     )
+    #                 except Exception as admin_e:
+    #                     logger.error(
+    #                         f"Ошибка при отправке уведомления администратору: {admin_e}"
+    #                     )
+    #             except TelegramBadRequest as e:
+    #                 logger.error(
+    #                     f"Ошибка при отправке уведомления о номере пользователю user_id={user_id}: {e}"
+    #                 )
+    #         else:
+    #             await message.answer("Ошибка при присвоении номера. Попробуйте снова.")
+    #     else:
+    #         await message.answer("Участник не найден.")
+    async def set_bib(event: [Message, CallbackQuery], state: FSMContext):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["set_bib_access_denied"])
             return
+        logger.info(f"Команда /set_bib от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
+        await message.answer(messages["wait_bib_ID"])
+        await state.set_state(RegistrationForm.waiting_for_bib)
+
+    @dp.message(RegistrationForm.waiting_for_bib)
+    async def process_set_bib(message: Message, state: FSMContext):
         parts = message.text.split()
-        if len(parts) != 3 or not parts[1].isdigit() or not parts[2].isdigit():
+        if len(parts) != 2 or not parts[0].isdigit() or not parts[1].isdigit():
             await message.answer(messages["set_bib_usage"])
             return
-        user_id = int(parts[1])
-        bib_number = int(parts[2])
+        user_id = int(parts[0])
+        bib_number = int(parts[1])
         if bib_number <= 0:
             await message.answer(messages["set_bib_invalid"])
             return
@@ -299,20 +436,32 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
         else:
             await message.answer("Участник не найден.")
 
-    @dp.message(Command("remove"))
-    async def remove_participant(message: Message):
-        logger.info(f"Команда /remove от user_id={message.from_user.id}")
-        if message.from_user.id != admin_id:
-            logger.warning(
-                f"Доступ к /remove запрещен для user_id={message.from_user.id}"
-            )
-            await message.answer(messages["remove_access_denied"])
+    @dp.message(Command("set_bib"))
+    async def cmd_set_bib(message: Message, state: FSMContext):
+        await set_bib(message, state)
+
+    @dp.callback_query(F.data == "admin_set_bib")
+    async def callback_set_bib(callback_query: CallbackQuery, state: FSMContext):
+        await set_bib(callback_query, state)
+
+    async def remove_participant(event: [Message, CallbackQuery], state: FSMContext):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["remove_access_denied"])
             return
-        parts = message.text.split()
-        if len(parts) != 2 or not parts[1].isdigit():
-            await message.answer(messages["remove_usage"])
-            return
-        user_id = int(parts[1])
+        logger.info(f"Команда /remove_participant от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
+        await message.answer(messages["wait_for_remove_id"])
+        await state.set_state(RegistrationForm.waiting_for_remove_id)
+
+    @dp.message(RegistrationForm.waiting_for_remove_id)
+    async def process_remove_participant(message: Message, state: FSMContext):
+        user_id = int(message.text)
         participant = get_participant_by_user_id(user_id)
         if participant:
             success = delete_participant(user_id)
@@ -359,15 +508,26 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
         else:
             await message.answer("Участник не найден.")
 
-    @dp.message(Command("export"))
-    async def export_participants(message: Message):
-        logger.info(f"Команда /export от user_id={message.from_user.id}")
-        if message.from_user.id != admin_id:
-            logger.warning(
-                f"Доступ к /export запрещен для user_id={message.from_user.id}"
-            )
-            await message.answer(messages["export_access_denied"])
+    @dp.message(Command("remove"))
+    async def cmd_remove(message: Message, state: FSMContext):
+        await remove_participant(message, state)
+
+    @dp.callback_query(F.data == "admin_remove")
+    async def callback_remove(callback_query: CallbackQuery, state: FSMContext):
+        await remove_participant(callback_query, state)
+
+    async def export_participants(event: [Message, CallbackQuery], state: FSMContext):
+        user_id = event.from_user.id
+        if user_id != admin_id:
+            await event.answer(messages["export_access_denied"])
             return
+        logger.info(f"Команда /export от user_id={user_id}")
+        if isinstance(event, CallbackQuery):
+            await event.message.delete()
+            message = event.message
+        else:
+            await event.delete()
+            message = event
         participants = get_all_participants()
         output = io.StringIO()
         delimiter = config.get("csv_delimiter", ";")
@@ -417,3 +577,13 @@ def register_admin_participant_handlers(dp: Dispatcher, bot: Bot, admin_id: int)
             document=BufferedInputFile(csv_bytes, filename="participants.csv")
         )
         logger.info(f"CSV-файл успешно отправлен для user_id={message.from_user.id}")
+
+    @dp.message(Command("export"))
+    async def cmd_export_participants(message: Message, state: FSMContext):
+        await export_participants(message, state)
+
+    @dp.callback_query(F.data == "admin_export")
+    async def callback_export_participants(
+        callback_query: CallbackQuery, state: FSMContext
+    ):
+        await export_participants(callback_query, state)
